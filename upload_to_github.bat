09d1be48eed5ca966360f2c61d9f0b3c755b9e12
qawsdegyu <mocvskhfssr@gmail.com>
2026-09-20 18:27:15 +0300

    تحديث اللوجو ومعاينة الرابط لموقع Vercel

--- upload_to_github.bat
+++ upload_to_github.bat
@@ -39,7 +39,7 @@ git add .
 :: 4. عمل Commit
 echo.
 echo [3/4] إنشاء الـ Commit...
-git commit -m "Initial commit - رفع موقع عش للعطور"
+git commit -m "تحديث الشعار وأيقونات الموقع ووسوم المشاركة والبحث (Logo, Favicon & Social Meta Tags)"
 
 :: 5. الرفع إلى GitHub
 echo.
